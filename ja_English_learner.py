9299f8c70575f50fb88ad46fbe6127f330341b72
Kiichi Mizugami <mizugami.kiichi.p4@dc.tohoku.ac.jp>
2025-12-04 11:15:28 +0900

    nativeLa = CHN

--- ja_English_learner.py
+++ ja_English_learner.py
@@ -38,7 +38,7 @@ print(" Model loaded successfully.\n")
 # プロンプト（日本人英語学習者の作文生成）
 # ==========================================
 prompt = """You are a English learner.
- Your native language is Japanese.
+ Your native language is Chinese.
  Please write an English essay .
  The essay should answer the following topic:
  
