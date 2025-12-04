d8e8bbf98c03ed82555344f25e071aa206d6a9c0
Kiichi Mizugami <mizugami.kiichi.p4@dc.tohoku.ac.jp>
2025-12-04 11:20:24 +0900

    Native = KOR

--- ja_English_learner.py
+++ ja_English_learner.py
@@ -14,7 +14,7 @@ os.environ["HF_HOME"] = os.path.expanduser("~/huggingface")
 os.environ["TRANSFORMERS_CACHE"] = os.path.expanduser("~/huggingface")
 
 # 出力ファイル
-output_file = "essays_Qwen3_CHN.txt"
+output_file = "essays_Qwen3_KOR.txt"
 
 # GPU設定
 device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -38,9 +38,10 @@ print(" Model loaded successfully.\n")
 # プロンプト（日本人英語学習者の作文生成）
 # ==========================================
 prompt = """You are a English learner.
- Your native language is Chinese.
+ Your native language is Korean.
  Please write an English essay .
  The essay should answer the following topic:
+ 
  "It is important for college students to have a part-time job."
  
  Write in simple English.
